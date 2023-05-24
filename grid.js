3140cb868a6f8712d50a8ce3d82d19cb1d57882b
Tatiana <tatiana_ivanova89@mail.ru>
2023-05-25 00:09:45 +0600

    #2 add tile with number

--- grid.js
+++ grid.js
@@ -2,7 +2,8 @@ import { Cell } from "./cell.js";
 
 const GRID_SIZE = 4;
 const CELLS_COUNT = GRID_SIZE * GRID_SIZE;
-export class Grid{
+
+export class Grid {
     constructor(gridElement) {
         this.cells = [];
         for (let i=0; i < CELLS_COUNT; i++) {
@@ -10,5 +11,11 @@ export class Grid{
                 new Cell(gridElement, i % GRID_SIZE, Math.floor(i/ GRID_SIZE))
             );
         }
+    }
+
+    getRandomEmptyCell() {
+        const emptyCells = this.cells.filter(cell => cell.isEmpty());
+        const randomIndex = Math.floor(Math.random() * emptyCells.length);
+        return emptyCells[randomIndex];
     }
 }
